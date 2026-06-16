2731d5e065d34d128ba06eefd7b9b72bbae20b81
Nimra Abid <abidnimra64@gmail.com>
2026-06-16 23:11:53 +0500

    Added CRUD operations for Inventory module

--- Book.java
+++ Book.java
@@ -2,4 +2,9 @@ public class Book {
     String title;
     String author;
     int quantity;
+
+    public void addBook() {}
+    public void viewBook() {}
+    public void updateBook() {}
+    public void deleteBook() {}
 }
